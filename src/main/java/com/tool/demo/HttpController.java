5ecc941c7be51e3b09889d974cad091c4e017b03
zhoulong <zhoul@bsfit.com.cn>
2019-07-25 11:42:29 +0800

    添加 返回 body 的接口

--- src/main/java/com/tool/demo/HttpController.java
+++ src/main/java/com/tool/demo/HttpController.java
@@ -1,6 +1,8 @@
 package com.tool.demo;
 
 
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +20,7 @@ import java.util.Enumeration;
 public class HttpController {
 
 
-    @RequestMapping("/query")
+    @RequestMapping("/query/total")
     public HttpDto getQuery(HttpServletRequest request) {
         request.getQueryString();
 
@@ -45,10 +47,13 @@ public class HttpController {
             }
         }
 
+        String body;
         try (InputStream is = request.getInputStream()) {
             if (is != null) {
-                String s = IOUtils.toString(is, StandardCharsets.UTF_8);
-                dto.setBody(s);
+                body = IOUtils.toString(is, StandardCharsets.UTF_8);
+                dto.setBody(body);
+
+                jsonPrint(body);
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -56,6 +61,40 @@ public class HttpController {
 
         dto.setContentType(request.getContentType());
 
+        log.info(JSONObject.toJSONString(dto, SerializerFeature.PrettyFormat));
+
         return dto;
+    }
+
+    @RequestMapping("/query/body")
+    public String getQueryBody(HttpServletRequest request) {
+
+        String body = "无body体";
+
+        try (InputStream is = request.getInputStream()) {
+            if (is != null) {
+                body = IOUtils.toString(is, StandardCharsets.UTF_8);
+                jsonPrint(body);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return body;
+    }
+
+    /**
+     * 强行解析，若不成功，catch 住即可
+     *
+     * @param body
+     */
+    private void jsonPrint(String body) {
+        try {
+            Object parse = JSONObject.parse(body);
+            String s = JSONObject.toJSONString(parse, SerializerFeature.PrettyFormat);
+            log.info(s);
+        } catch (Exception e) {
+            //忽略即可
+        }
     }
 }
